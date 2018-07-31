23b253b04a49e176694566c41be5a905af2e48e6
chrissutanto <chris.sutanto28@gmail.com>
2018-07-30 22:14:40 -0700

    Added api info

--- info-display.py
+++ info-display.py
@@ -1,8 +1,17 @@
 #!usr/bin/python
 import time
 import Adafruit_CharLCD as LCD
+import json
+import requests
 
-# pin configuration
+# translink API info
+api_key = "fQkcNy0xwMb4e3uWUwlD"
+api_url_base = "http://api.translink.ca/rttapi/v1"
+
+# desired bus stop
+stop_no = 51204
+
+# GPIO pin configuration on Raspberry Pi Zero W
 lcd_rs = 26
 lcd_en = 19
 lcd_d4 = 13
@@ -18,7 +27,7 @@ lcd_rows = 2
 # initialize screen
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-# test: if I set variable, print to screen, then change variable, will printed value change?
+
 
 message = "first\nmessage"
 
